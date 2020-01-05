ac7f54b5e800d32cdec1381e19e059283c34619b
Gerry Sousa <gerry.nett@gmail.com>
2020-01-05 20:24:02 -0300

    Add testes Test_ObterInformacoesDeTodosOsProblemaComSucesso

---
+++ DesafioAutomacaoApiRest/Requests/Issues/GetAllIssuesRequest.cs
@@ -0,0 +1,23 @@
+﻿using DesafioAutomacaoApiRest.Bases;
+using DesafioAutomacaoApiRest.Helpers;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioAutomacaoApiRest.Requests.Issues
+{
+    class GetAllIssuesRequest : RequestBase
+    {
+        public GetAllIssuesRequest(int pageSize, int page)
+        {
+            requestService = "/api/rest/issues?page_size="+pageSize+"&page=" + page;
+            method = Method.GET;
+            
+            headers.Add("Authorization", Global.token);
+        }
+    }
+}
--- DesafioAutomacaoApiRest/Tests/Issues/IssuesTests.cs
+++ DesafioAutomacaoApiRest/Tests/Issues/IssuesTests.cs
@@ -51,5 +51,52 @@ namespace DesafioAutomacaoApiRest.Tests.Issues
 
             #endregion
         }
+
+        [Test]
+        public void Test_ObterInformacoesDeTodosOsProblemaComSucesso()
+        {
+            #region Parameters
+            string statusEsperado = "OK";
+            int pageSize = 10;
+            int page = 1;
+
+            int idIssue1 = 1;
+            string summary1 = "Descrição simples";
+            string description1 = "Descrição detalhada ssss";
+
+            int idIssue2 = 2;
+            string summary2 = "This is a test issue";
+            string description2 = "This is a test description";
+            #endregion
+
+            #region Acoes
+            GetAllIssuesRequest getAllIssuesRequest = new GetAllIssuesRequest(pageSize, page);
+            IRestResponse<dynamic> response = getAllIssuesRequest.ExecuteRequest();
+            #endregion
+
+            #region Asserts
+            int idResposta1 = response.Data["issues"][1]["id"];
+            string summaryResposta1 = response.Data["issues"][1]["summary"];
+            string descriptionResposta1 = response.Data["issues"][1]["description"];
+
+            int idResposta2 = response.Data["issues"][0]["id"];
+            string summaryResposta2 = response.Data["issues"][0]["summary"];
+            string descriptionResposta2 = response.Data["issues"][0]["description"];
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(statusEsperado, response.StatusCode.ToString());
+
+                Assert.AreEqual(idIssue1, idResposta1);
+                Assert.AreEqual(summary1, summaryResposta1);
+                Assert.AreEqual(description1, descriptionResposta1);
+
+                Assert.AreEqual(idIssue2, idResposta2);
+                Assert.AreEqual(summary2, summaryResposta2);
+                Assert.AreEqual(description2, descriptionResposta2);
+            });
+
+            #endregion
+        }
     }
 }
