594da13dfa6db5ad40cc8c2ec8f2d2250c000d06
Gerry Sousa <gerry.nett@gmail.com>
2020-01-19 18:13:17 -0300

    Add Test_ObterConfiguracaoDoCSVComSucesso

---
+++ DesafioAutomacaoApiRest/Requests/Config/GetConfigurationOptionRequest.cs
@@ -0,0 +1,23 @@
+﻿using DesafioAutomacaoApiRest.Bases;
+using DesafioAutomacaoApiRest.Helpers;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioAutomacaoApiRest.Requests.Config
+{
+    class GetConfigurationOptionRequest : RequestBase
+    {
+        public GetConfigurationOptionRequest(string optionParametro)
+        {
+            requestService = "/api/rest/config?option="+ optionParametro;
+            method = Method.GET;
+
+            headers.Add("Authorization", Global.token);
+            parameters.Add("option", optionParametro);
+        }
+    }
+}
---
+++ DesafioAutomacaoApiRest/Tests/Configs/ConfigsTests.cs
@@ -0,0 +1,45 @@
+﻿using DesafioAutomacaoApiRest.Bases;
+using DesafioAutomacaoApiRest.Helpers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using DesafioAutomacaoApiRest.Pages;
+using DesafioAutomacaoApiRest.Requests.Projects;
+using NUnit.Framework;
+using RestSharp;
+using DesafioAutomacaoApiRest.Requests.Config;
+
+namespace DesafioAutomacaoApiRest.Tests.Configs
+{
+    class ConfigsTests : TestBase
+    {
+        [Test]
+        public void Test_ObterConfiguracaoDoCSVComSucesso()
+        {
+            #region Parameters
+            string statusEsperado = "OK";
+            string option = "csv_separator";
+            string valorEsperado = ",";
+            #endregion
+
+            #region Acoes
+            GetConfigurationOptionRequest getConfigurationOptionRequest = new GetConfigurationOptionRequest(option);
+            IRestResponse<dynamic> response = getConfigurationOptionRequest.ExecuteRequest();
+            #endregion
+
+            #region Asserts
+            string optionResposta = response.Data["configs"][0]["option"];
+            string valorResposta = response.Data["configs"][0]["value"];
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(statusEsperado, response.StatusCode.ToString());
+                Assert.AreEqual(valorEsperado, valorResposta);
+                Assert.AreEqual(option, optionResposta);
+            });
+
+            #endregion
+        }
+
+
+    }
+}
